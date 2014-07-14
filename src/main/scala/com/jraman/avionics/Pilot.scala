34acb0970d286cef8b67d4c5c0d01e1e9a619924
jayanth <jraman@users.noreply.github.com>
2014-07-14 01:36:32 -0700

    Sec 7.4 ActorContexts Add Pilot, CoPilot & Autopilot

---
+++ src/main/scala/com/jraman/avionics/Pilot.scala
@@ -0,0 +1,66 @@
+package com.jraman.avionics
+
+import akka.actor.{ActorLogging, Actor, ActorRef}
+import com.jraman.avionics.Plane.Controls
+
+/**
+ * Pilot
+ */
+
+object Pilot {
+  // since there are no params we create case objects instead of case classes
+  case object ReadyToGo
+  case object RelinquishControl
+}
+
+
+class Pilot extends Actor {
+  import Pilot._
+  import Plane.GiveMeControl
+
+  var controls: ActorRef = context.system.deadLetters
+  var copilot: ActorRef = context.system.deadLetters
+  var autopilot: ActorRef = context.system.deadLetters
+
+  val copilotName = context.system.settings.config.getString(
+    "com.jraman.avionics.flightcrew.copilotName"
+  )
+
+  def receive = {
+    case ReadyToGo =>
+      context.parent ! GiveMeControl
+      copilot = context.actorFor("../" + copilotName)
+      autopilot = context.actorFor("../Autopilot")
+
+    case Controls(controlSurfaces) =>
+      controls = controlSurfaces
+  }
+}
+
+
+class CoPilot extends Actor {
+  import Pilot.ReadyToGo
+
+  var controls: ActorRef = context.system.deadLetters
+  var pilot: ActorRef = context.system.deadLetters
+  var autopilot: ActorRef = context.system.deadLetters
+
+  val pilotName = context.system.settings.config.getString(
+    "com.jayanth.avionics.flightcrew.pilotName"
+  )
+
+  def receive = {
+    case ReadyToGo =>
+      pilot = context.actorFor("../" + pilotName)
+      autopilot = context.actorFor("../Autopilot")
+  }
+}
+
+
+class Autopilot extends Actor with ActorLogging {
+  // empty for now
+  def receive = {
+    case msg =>
+      log.info(s"Got $msg")
+  }
+}
--- src/main/scala/com/jraman/avionics/Plane.scala
+++ src/main/scala/com/jraman/avionics/Plane.scala
@@ -1,11 +1,13 @@
 package com.jraman.avionics
 
-import akka.actor.{Props, Actor, ActorLogging}
+import akka.actor.{ActorRef, Props, Actor, ActorLogging}
 
 object Plane {
   // Returns the control surface to the Actor that
   // asks for them
   case object GiveMeControl
+  // Response to the GiveMeControl message
+  case class Controls(controls: ActorRef)
 }
 
 // We want the Plane to own the Altimeter and we're going to
@@ -20,15 +22,25 @@ class Plane extends Actor with ActorLogging {
     Props(Altimeter()), "Altimeter")
   val controls = context.actorOf(
     Props(new ControlSurfaces(altimeter)), "ControlSurfaces")
+  val cfgstr = "com.jraman.avionics.flightcrew"
+  val config = context.system.settings.config
+  val pilot = context.actorSelection("/user/Plane/Pilot/" + config.getString(s"$cfgstr.pilotName"))
+  val copilot = context.actorSelection("/user/Plane/Pilot/" + config.getString(s"$cfgstr.coPilotName"))
+  val autopilot = context.actorSelection("/user/Plane/Pilot/Autopilot")
+  val flightAttendant = context.actorOf(Props(LeadFlightAttendant()),
+                                        config.getString(s"$cfgstr.leadAttendantName"))
 
   override def preStart() = {
+    // Register self to receive altimeter updates
     altimeter ! RegisterListener(self)
+    List(pilot, copilot) foreach (_ ! Pilot.ReadyToGo)
   }
 
   def receive = {
     case GiveMeControl =>
       log info("Plane giving control.")
-      sender ! controls
+      sender ! Controls(controls)
+
     case AltitudeUpdate(altitude) =>
       log info(s"Altitude is now: $altitude")
   }
